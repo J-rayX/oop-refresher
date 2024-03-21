d0d381b9b05e05e4ee000af26d906e6d3c3e1006
Jekayin-Oluwa Olabemiwo <jkaylight@gmail.com>
2024-03-21 17:05:39 +0100

    Class Inheritance as is-a relationship

---
+++ inheritance-excercise.py
@@ -0,0 +1,23 @@
+class Employee:
+  MIN_SALARY = 30000    
+
+  def __init__(self, name, salary=MIN_SALARY):
+      self.name = name
+      if salary >= Employee.MIN_SALARY:
+        self.salary = salary
+      else:
+        self.salary = Employee.MIN_SALARY
+  def give_raise(self, amount):
+    self.salary += amount      
+        
+
+class Manager(Employee):
+  def display(self):
+    print('Manager', self.name)
+
+
+mng = Manager("Debbie Lashko", 86500)
+print(mng.name)
+mng.display()
+
+print(isinstance(mng, Employee))
